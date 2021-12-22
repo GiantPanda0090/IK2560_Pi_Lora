f56066feb4aed4055bde4e53dd2713417f4bdc46
Qi Li <qi5@kth.se>
2021-12-23 00:29:13 +0100

    calculation correction

--- data_collection/Plot.m
+++ data_collection/Plot.m
@@ -12,17 +12,6 @@ for i=1:length(files)
     current_file = load(strcat(files(i).folder,'\',files(i).name), '-ascii')
     data_table = [data_table;current_file]
     data_table=round(data_table,3)
-
-    %longitude=current_file(:,3);
-    %latitude=current_file(:,4);
-    %initialx = current_file(:,1);
-    %initialy = current_file(:,2);
-    %dist =mean(current_file(:,1),1);
-    %dist = [dist,distance(initialx,initialy,latitude,longitude)]
-    %rssi=mean(current_file(:,2),1);
-    %packet_rssi = mean(current_file(:,3),1);
-    %snr= mean(current_file(:,4),1);
-    %data_table = [data_table;[dist,rssi,packet_rssi,snr]]
 end
 dist=data_table(:,1)
 rssi=data_table(:,2)
@@ -31,14 +20,57 @@ snr = data_table(:,4)
 %A = unique(table(dist,rssi,packet_rssi,snr))
 %uni_dist = unique(A.dist)
 
+%Delete Min Max
+[ii,jj,kk]=unique(data_table(:,1))
+distance_avg = ii
+rssi_max = [ii accumarray(kk,data_table(:,2),[],@max)]
+rssi_min = [ii accumarray(kk,data_table(:,2),[],@min)]
+rssi_min_lst = find(data_table(:,1)==rssi_min(kk,1) & data_table(:,2)==rssi_min(kk,2))
+rssi_max_lst = find(data_table(:,1)==rssi_max(kk,1) & data_table(:,2)==rssi_max(kk,2))
+lst = [rssi_min_lst;rssi_max_lst]
+data_table(lst,:)=[]
+[ii,jj,kk]=unique(data_table(:,1))
+distance_avg = ii
+prssi_max = [ii accumarray(kk,data_table(:,3),[],@max)]
+prssi_min = [ii accumarray(kk,data_table(:,3),[],@min)]
+prssi_min_lst = find(data_table(:,1)==prssi_min(kk,1) & data_table(:,3)==prssi_min(kk,2))
+prssi_max_lst = find(data_table(:,1)==prssi_max(kk,1) & data_table(:,3)==prssi_max(kk,2))
+lst = [prssi_min_lst;prssi_max_lst]
+data_table(lst,:)=[]
+
+%load expierment data
 [ii,jj,kk]=unique(data_table(:,1))
 distance_avg = ii
 rssi_avg=[ii accumarray(kk,data_table(:,2),[],@mean)]
 rssi_avg=rssi_avg(:,2)
+lower=[ii accumarray(kk,data_table(:,2),[], @(x) quantile(x,.05))]
+upper=[ii accumarray(kk,data_table(:,2),[], @(x) quantile(x,.95))]
+lower_rssi=lower(:,2)
+upper_rssi=upper(:,2)
 packet_rssi_avg=[ii accumarray(kk,data_table(:,3),[],@mean)]
 packet_rssi_avg=packet_rssi_avg(:,2)
+lower=[ii accumarray(kk,data_table(:,3),[], @(x) quantile(x,.05))]
+upper=[ii accumarray(kk,data_table(:,3),[], @(x) quantile(x,.95))]
+lower_prssi=lower(:,2)
+upper_prssi=upper(:,2)
 snr_avg=[ii accumarray(kk,data_table(:,4),[],@mean)]
 snr_avg=snr_avg(:,2)
+
+lowerthanzero = find(snr_avg<0);
+upperthanzero = find(snr_avg>0);
+CI95_lower=double.empty(1,0);
+CI95_upper=double.empty(1,0);
+
+for i=1:length(lower_prssi)
+    if ismember(i,lowerthanzero)
+        CI95_lower = [CI95_lower;lower_prssi(i)]
+        CI95_upper = [CI95_upper;upper_prssi(i)]
+    else
+       CI95_lower = [CI95_lower;lower_rssi(i)]
+       CI95_upper = [CI95_upper;upper_rssi(i)]
+    end
+end
+
 
 if snr_avg < 0
     packet_strength= packet_rssi_avg + snr_avg * 0.25
@@ -50,7 +82,10 @@ figure(1);
 avg_power = packet_strength
 xdata = distance_avg(:)
 ydata = avg_power
-plot(xdata,ydata,'-o')
+err = CI95_upper - CI95_lower
+errorbar(xdata,ydata,err)
+set(gca,'YScale','log')
+set(gca,'XScale','log')
 
 
 
@@ -58,20 +93,22 @@ cf = fit(xdata,ydata,'poly2');
 hold on
 plot(cf,'--')
 legend('Power','Best Fit')
-xlabel('Distance(km)'), ylabel('Power(dbm)')
-title('Power Against Distance')
+xlabel('Distance(m)'), ylabel('Power(dbm)')
+title('Power Against Distance (Log Scale)')
 
 out = gca;
-exportgraphics(out,'result/graph/distance_path_lost.png','Resolution',500)
+exportgraphics(out,'result/graph/distance_power.png','Resolution',500)
 
 figure(2);
 %RSSI(avg_power) = init_power +gain − Ldb +gain dBm.
 %Ldb = init_power +gain -RSSI(avg_power)+gain 
 init_power = 13
 gain = 17
-transmit_power = init_power+gain
-Ldb = transmit_power - avg_power -gain
-plot(xdata,Ldb,'-')
+transmit_power = init_power+(gain+2.51)
+Ldb = transmit_power - avg_power-(gain+2.51)
+errorbar(xdata,Ldb,err)
+set(gca,'YScale','log')
+set(gca,'XScale','log')
 
 myfittype = fittype('a +10*n*log10(d*10^3) -b',...
     'dependent',{'y'},'independent',{'d'},...
@@ -80,12 +117,12 @@ cf = fit(xdata,Ldb,myfittype);
 hold on
 plot(cf,'--')
 legend('Path Lost','Best Fit')
-xlabel('Distance(km)'), ylabel('Power(dbm)')
-title('Path Lost Against Distance')
+xlabel('Distance(m)'), ylabel('Power(dbm)')
+title('Path Lost Against Distance(Log Scale)')
 
 
 out = gca;
-exportgraphics(out,'result/graph/distance_power.png','Resolution',500)
+exportgraphics(out,'result/graph/distance_path_lost.png','Resolution',500)
 
 %Equition Solver for N
 %n = (20*log(f)-147.58 - Ldb)/10 /log(distance)
@@ -115,16 +152,18 @@ figure(3);
 receive_power_lst = transmit_power-Ldb
 rssi_lst = receive_power_lst+gain
 for i=1:length(rssi_avg)
-    cur_path=[init_power;transmit_power;receive_power_lst(i);rssi_lst(i);rssi_lst(i)];
+    cur_path=[init_power;transmit_power;transmit_power+2.15;receive_power_lst(i);receive_power_lst(i)+2.15;rssi_lst(i);rssi_lst(i)];
     path = [path,cur_path];
 end
-x_path = {'1 - TX Radio','2 - PA TX','3 - Path Loss','4 - PA RX','5 - RSSI'}
+x_path = {'1 - TX Radio','2 - PA TX','3 - Antenna Gain RX','4 - Path Loss','5 - Antenna Gain RX','6 - PA RX','7 - RSSI'}
 C = categorical(x_path)
 
 plot(C,path)
+legendCell = cellstr(num2str(xdata, '%0.1d'));
+legend(legendCell)
 xlabel('Stage of the Transmission'), ylabel('Power(dbm)')
 title('Power Development Along the Radio Path Per Distance Unit')
 out = gca;
-exportgraphics(out,'result/graph/power_path.png','Resolution',500)
+exportgraphics(out,'result/graph/power_path.png','Resolution',700)
 
 
